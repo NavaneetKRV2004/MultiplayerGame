96acc67ac36a6a15387425125de28e2435a17414
NavaneetKRV2004 <navateen@gmail.com>
2026-01-27 22:53:14 +0530

    tiny patch

--- botw ripoff conbverted/UI/players_list.gd
+++ botw ripoff conbverted/UI/players_list.gd
@@ -13,4 +13,5 @@ func _process(delta: float) -> void:
 		$VBoxContainer/Label2.text="Players " +str(len(world.players))
 		$VBoxContainer/Panel/Label.text=""
 		for i in world.players:
-			$VBoxContainer/Panel/Label.text+=world.players[i].Player_name+(" [color=GREEN](you)[/color]" if world.players[i]==world.my_player else "")+"\n"
+			if world.players[i]:
+				$VBoxContainer/Panel/Label.text+=world.players[i].Player_name+(" [color=GREEN](you)[/color]" if world.players[i]==world.my_player else "")+"\n"
